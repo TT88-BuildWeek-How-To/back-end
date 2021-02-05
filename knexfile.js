c6319ff71dc8370735a6ed084ffcaf637bcc16e0
ShirleyLe3 <synchrotronflux@yahoo.com>
2021-02-04 22:15:12 -0500

    deployment issues

--- knexfile.js
+++ knexfile.js
@@ -37,18 +37,5 @@ module.exports = {
   },
   //add a production
   
-  production: {
-    client: "pg",
-    connection: process.env.DATABASE_URL,
-    migrations: {
-      directory: "./data/migrations",
-    },
-    seeds: {
-      directory: './data/seeds'
-    },
-    pool: {
-      min: 2,
-      max: 10
-    },
-  }
+  
 };
