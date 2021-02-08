bd4f5af02d5697b8dcbc8d28e572b69e84b1d8bb
Shirley Lee <synchrotronflux@yahoo.com>
2021-02-07 22:03:49 -0500

    Final Submission

--- data/dbconfig.js
+++ data/dbconfig.js
@@ -5,4 +5,30 @@ const knex = require('knex')
 const knexfile = require('../knexfile')
 const environment = process.env.NODE_ENV || "development"
 
-module.exports = knex(knexfile[environment])
+module.exports = knex({
+    client: "pg",
+    connection: {
+        host : process.env.DB_HOST,
+        user :process.env.DB_USER,
+        password : process.env.DB_PASSWORD,
+        name : process.env.DB_NAME,
+        port : process.env.DB_PORT,
+        ssl: {
+            rejectUnauthorized: false
+        }
+        
+    },
+    migrations: {
+        directory: "./data/migrations",
+    },
+    seeds: {
+        directory: "./data/seeds",
+    },
+    pool: {
+        min: 2,
+        max: 10
+    },
+})
+    
+    
+    // knexfile[environment])
--- knexfile.js
+++ knexfile.js
@@ -36,6 +36,27 @@ module.exports = {
 
   },
   //add a production
-  
-  
+  production: {
+    client: "pg",
+    connection: {
+      host : process.env.DB_HOST,
+      user :process.env.DB_USER,
+      password : process.env.DB_PASSWORD,
+      name : process.env.DB_NAME,
+      port : process.env.DB_PORT,
+      ssl: {
+          rejectUnauthorized: false
+},        
+},
+migrations: {
+    directory: "./data/migrations",
+},
+seeds: {
+    directory: "./data/seeds",
+},
+pool: {
+    min: 2,
+    max: 10
+},
+},  
 };
